03489dbec4b3bc9412a2810f52d9e0863e0d8648
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2017-04-17 19:21:44 +0200

    Init

---
+++ android/build/generated/jni/ti.versioncode.VersioncodeModule.cpp
@@ -0,0 +1,173 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2011-2016 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+
+/** This code is generated, do not edit by hand. **/
+
+#include "ti.versioncode.VersioncodeModule.h"
+
+#include "AndroidUtil.h"
+#include "EventEmitter.h"
+#include "JNIUtil.h"
+#include "JSException.h"
+#include "Proxy.h"
+#include "ProxyFactory.h"
+#include "TypeConverter.h"
+#include "V8Util.h"
+
+
+
+
+#include "org.appcelerator.kroll.KrollModule.h"
+
+#define TAG "VersioncodeModule"
+
+using namespace v8;
+
+namespace ti {
+namespace versioncode {
+
+
+Persistent<FunctionTemplate> VersioncodeModule::proxyTemplate;
+jclass VersioncodeModule::javaClass = NULL;
+
+VersioncodeModule::VersioncodeModule(jobject javaObject) : titanium::Proxy(javaObject)
+{
+}
+
+void VersioncodeModule::bindProxy(Local<Object> exports, Local<Context> context)
+{
+	Isolate* isolate = context->GetIsolate();
+
+	Local<FunctionTemplate> pt = getProxyTemplate(isolate);
+	Local<Function> proxyConstructor = pt->GetFunction(context).ToLocalChecked();
+	Local<String> nameSymbol = NEW_SYMBOL(isolate, "Versioncode"); // use symbol over string for efficiency
+	Local<Object> moduleInstance = proxyConstructor->NewInstance(context).ToLocalChecked();
+	exports->Set(nameSymbol, moduleInstance);
+}
+
+void VersioncodeModule::dispose(Isolate* isolate)
+{
+	LOGD(TAG, "dispose()");
+	if (!proxyTemplate.IsEmpty()) {
+		proxyTemplate.Reset();
+	}
+
+	titanium::KrollModule::dispose(isolate);
+}
+
+Local<FunctionTemplate> VersioncodeModule::getProxyTemplate(Isolate* isolate)
+{
+	if (!proxyTemplate.IsEmpty()) {
+		return proxyTemplate.Get(isolate);
+	}
+
+	LOGD(TAG, "GetProxyTemplate");
+
+	javaClass = titanium::JNIUtil::findClass("ti/versioncode/VersioncodeModule");
+	EscapableHandleScope scope(isolate);
+
+	// use symbol over string for efficiency
+	Local<String> nameSymbol = NEW_SYMBOL(isolate, "Versioncode");
+
+	Local<FunctionTemplate> t = titanium::Proxy::inheritProxyTemplate(isolate,
+		titanium::KrollModule::getProxyTemplate(isolate)
+, javaClass, nameSymbol);
+
+	proxyTemplate.Reset(isolate, t);
+	t->Set(titanium::Proxy::inheritSymbol.Get(isolate),
+		FunctionTemplate::New(isolate, titanium::Proxy::inherit<VersioncodeModule>)->GetFunction());
+
+	titanium::ProxyFactory::registerProxyPair(javaClass, *t);
+
+	// Method bindings --------------------------------------------------------
+	titanium::SetProtoMethod(isolate, t, "getVersionsCode", VersioncodeModule::getVersionsCode);
+
+	Local<ObjectTemplate> prototypeTemplate = t->PrototypeTemplate();
+	Local<ObjectTemplate> instanceTemplate = t->InstanceTemplate();
+
+	// Delegate indexed property get and set to the Java proxy.
+	instanceTemplate->SetIndexedPropertyHandler(titanium::Proxy::getIndexedProperty,
+		titanium::Proxy::setIndexedProperty);
+
+	// Constants --------------------------------------------------------------
+
+	// Dynamic properties -----------------------------------------------------
+
+	// Accessors --------------------------------------------------------------
+
+	return scope.Escape(t);
+}
+
+// Methods --------------------------------------------------------------------
+void VersioncodeModule::getVersionsCode(const FunctionCallbackInfo<Value>& args)
+{
+	LOGD(TAG, "getVersionsCode()");
+	Isolate* isolate = args.GetIsolate();
+	HandleScope scope(isolate);
+
+	JNIEnv *env = titanium::JNIScope::getEnv();
+	if (!env) {
+		titanium::JSException::GetJNIEnvironmentError(isolate);
+		return;
+	}
+	static jmethodID methodID = NULL;
+	if (!methodID) {
+		methodID = env->GetMethodID(VersioncodeModule::javaClass, "getVersionsCode", "()Lorg/appcelerator/kroll/KrollDict;");
+		if (!methodID) {
+			const char *error = "Couldn't find proxy method 'getVersionsCode' with signature '()Lorg/appcelerator/kroll/KrollDict;'";
+			LOGE(TAG, error);
+				titanium::JSException::Error(isolate, error);
+				return;
+		}
+	}
+
+	Local<Object> holder = args.Holder();
+	// If holder isn't the JavaObject wrapper we expect, look up the prototype chain
+	if (!JavaObject::isJavaObject(holder)) {
+		holder = holder->FindInstanceInPrototypeChain(getProxyTemplate(isolate));
+	}
+
+	titanium::Proxy* proxy = titanium::Proxy::unwrap(holder);
+
+	jvalue* jArguments = 0;
+
+	jobject javaProxy = proxy->getJavaObject();
+	jobject jResult = (jobject)env->CallObjectMethodA(javaProxy, methodID, jArguments);
+
+
+
+	if (!JavaObject::useGlobalRefs) {
+		env->DeleteLocalRef(javaProxy);
+	}
+
+
+
+	if (env->ExceptionCheck()) {
+		Local<Value> jsException = titanium::JSException::fromJavaException(isolate);
+		env->ExceptionClear();
+		return;
+	}
+
+	if (jResult == NULL) {
+		args.GetReturnValue().Set(Null(isolate));
+		return;
+	}
+
+	Local<Value> v8Result = titanium::TypeConverter::javaObjectToJsValue(isolate, env, jResult);
+
+	env->DeleteLocalRef(jResult);
+
+
+	args.GetReturnValue().Set(v8Result);
+
+}
+
+// Dynamic property accessors -------------------------------------------------
+
+
+} // versioncode
+} // ti
